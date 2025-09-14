47a114dff0dabb5f67a8addbceda911d3ea39dab
Jessie King <jessie.kinganga@gmail.com>
2025-09-14 21:20:13 +0100

    VPC Setup

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "mainvpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = { Name = var.vpc_name }
+}
+
+resource "aws_subnet" "az1" {
+  availability_zone       = var.availability_zones[0]
+  cidr_block              = var.public_subnet_cidrs[0]
+  vpc_id                  = aws_vpc.mainvpc.id
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-az1" }
+}
+
+resource "aws_subnet" "az2" {
+  availability_zone       = var.availability_zones[1]
+  cidr_block              = var.public_subnet_cidrs[1]
+  vpc_id                  = aws_vpc.mainvpc.id
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-az2" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.mainvpc.id
+  tags   = { Name = "internet-gateway" }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.mainvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "main-route-table" }
+}
+
+resource "aws_route_table_association" "route_table_az1" {
+  subnet_id      = aws_subnet.az1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "route_table_az2" {
+  subnet_id      = aws_subnet.az2.id
+  route_table_id = aws_route_table.route_table.id
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,29 @@ variable "region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "vpc_name" {
+  description = "VPC name."
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets."
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "AZs assigned to the VPC subnets"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Resource tag key-value pairs."
+  type        = map(string)
+  default     = {}
+}

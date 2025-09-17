6d935f4086d61c59015e65d6eeeb01914071353d
Jessie King <jessie.kinganga@gmail.com>
2025-09-17 09:31:03 +0100

    Updated tags

--- terraform/modules/ecr/output.tf
+++ terraform/modules/ecr/output.tf
@@ -2,13 +2,3 @@ output "repository_url" {
   description = "The URL of the ECR repository"
   value       = aws_ecr_repository.this.repository_url
 }
-
-output "repository_arn" {
-  description = "ECR repository ARN"
-  value       = aws_ecr_repository.private_ecr.arn
-}
-
-output "repository_name" {
-  description = "The name of the ECR repository"
-  value       = aws_ecr_repository.this.name
-}

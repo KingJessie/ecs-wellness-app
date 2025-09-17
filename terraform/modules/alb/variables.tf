317e51e16e7fff4b7642ea7f35f7ff3961fac863
Jessie King <jessie.kinganga@gmail.com>
2025-09-17 13:35:53 +0100

    ALB setup and update to root module, output and variables

---
+++ terraform/modules/alb/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  description = "The name of the project."
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs."
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Resource tag key-value pairs"
+  type        = map(string)
+  default = {
+    project = "defectdojo-app"
+    env     = "dev"
+    Owner   = "jessie.king"
+  }
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC."
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "The ARN of the ACM certificate for HTTPS."
+  type        = string
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,23 +1,59 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
 output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnet_ids
 }
 
 output "ecs_cluster_name" {
-  value = module.ecs.cluster_name
+  description = "ECS cluster name"
+  value       = module.ecs.cluster_name
 }
 
 output "ecs_service_name" {
-  value = module.ecs.service_name
+  description = "ECS service name"
+  value       = module.ecs.service_name
 }
 
 output "ecr_repository_url" {
-  value = module.ecr.repository_url
+  description = "ECR repository URL"
+  value       = module.ecr.repository_url
 }
 
 output "ecs_instance_profile_name" {
-  value = module.iam.ecs_instance_profile_name
+  description = "ECS instance profile name"
+  value       = module.iam.ecs_instance_profile_name
 }
 
 output "ecs_task_execution_role_arn" {
-  value = module.iam.ecs_task_execution_role_arn
+  description = "ECS task execution role ARN"
+  value       = module.iam.ecs_task_execution_role_arn
+}
+
+output "alb_sg_id" {
+  description = "ALB security group ID"
+  value       = module.alb.alb_sg_id
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value       = module.alb.alb_dns_name
+}
+
+output "alb_arn" {
+  description = "ALB ARN"
+  value       = module.alb.alb_arn
+}
+
+output "alb_zone_id" {
+  description = "ALB hosted zone ID"
+  value       = module.alb.alb_zone_id
+}
+
+output "http_tg_arn" {
+  description = "HTTP target group ARN"
+  value       = module.alb.http_tg_arn
 }

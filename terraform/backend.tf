63989dbdf9d51f83101d1ede884bb2d89746f5a6
Jessie King <jessie.kinganga@gmail.com>
2025-09-17 22:40:32 +0100

    Updated version to allow Dynamodb

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "defectdojo-5756895-tfstate"
-    key          = "ecs-project/dev/terraform.tfstate"
-    region       = "eu-west-2"
-    use_lockfile = true
-    encrypt      = true
+    bucket         = "defectdojo-5756895-tfstate"
+    key            = "ecs-project/dev/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "tf-lock-table"
+    encrypt        = true
   }
 }

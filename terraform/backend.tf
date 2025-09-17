5c29b2359d513e319108d2f3b46bd7a7bd075938
Jessie King <jessie.kinganga@gmail.com>
2025-09-17 12:26:25 +0100

    updated root module and s3 bucket with unique name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "defectdojo-dev-tfstate"
+    bucket       = "defectdojo-5756895-tfstate"
     key          = "ecs-project/dev/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,5 @@
 module "vpc" {
-  source = "./modules/vpc"
-
+  source              = "./modules/vpc"
   vpc_name            = var.vpc_name
   vpc_cidr            = var.vpc_cidr
   public_subnet_cidrs = var.public_subnet_cidrs
@@ -19,7 +18,24 @@ module "iam" {
   source       = "./modules/iam"
   project_name = var.project_name
   tags         = var.tags
+}
 
+
+module "ecs" {
+  source             = "./modules/ecs"
+  project_name       = var.project_name
+  tags               = var.tags
+  availability_zones = var.availability_zones
+  region             = var.region
+  image_tag          = var.image_tag
+
+  vpc_id                      = module.vpc.vpc_id
+  public_subnet_ids           = module.vpc.public_subnet_ids
+  ecs_task_execution_role_arn = module.iam.ecs_task_execution_role_arn
+  ecs_instance_profile_name   = module.iam.ecs_instance_profile_name
+  repository_url              = module.ecr.repository_url
+  alb_sg_id                   = module.alb.alb_sg_id
+  http_tg_arn                 = module.alb.http_tg_arn
 }
 
 

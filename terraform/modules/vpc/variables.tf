f8e0968e78550fa3077a327f098efd1c3dca8e4c
Jessie King <jessie.kinganga@gmail.com>
2025-09-17 09:39:40 +0100

    Updated variable and output file

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -3,6 +3,17 @@ variable "vpc_name" {
   type        = string
 }
 
+variable "vpc_id" {
+  description = "The ID of the VPC."
+  type        = string
+  
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs."
+  type        = list(string)
+}
+
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC."
   type        = string

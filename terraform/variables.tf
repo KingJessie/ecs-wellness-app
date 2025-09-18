306961b46156bed980f2293acc1a65c386543d59
Jessie King <jessie.kinganga@gmail.com>
2025-09-18 13:58:01 +0100

    Variables for plan workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,42 +7,52 @@ variable "region" {
 variable "project_name" {
   description = "Project name."
   type        = string
+  default     = "ddojo"
 }
 
 variable "vpc_name" {
   description = "VPC name."
   type        = string
+  default     = "main-vpc"
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC."
   type        = string
+  default     = "10.1.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets."
   type        = list(string)
+  default     = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 
 variable "availability_zones" {
   description = "AZs assigned to the VPC subnets"
   type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
 }
 
 variable "tags" {
   description = "Resource tag key-value pairs."
   type        = map(string)
-  default     = {}
+  default     = {
+    "Environment" = "dev"
+    "Owner"       = "jessie.king"}
+
 }
 
 variable "domain_name" {
   description = "The domain name"
   type        = string
+  default     = "jessking-lab.com"
 }
 
 variable "hosted_zone_name" {
   description = "Route53 Public hosted zone name"
   type        = string
+  default     = "jessking-lab.com"
 }
 
 variable "image_tag" {

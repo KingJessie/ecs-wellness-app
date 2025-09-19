dfc53a9851c91e98f2f9606b47f555803348ef5d
Jessie King <jessie.kinganga@gmail.com>
2025-09-19 17:08:51 +0100

    Update port

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -34,11 +34,11 @@ resource "aws_security_group" "alb_sg" {
   }
 
   egress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = [var.vpc_cidr]
-    description = "ALB to ECS tasks on 3000"
+    description = "ALB to ECS tasks on 80"
   }
 }
 
@@ -48,7 +48,7 @@ resource "aws_security_group" "alb_sg" {
 resource "aws_lb_target_group" "http_tg" {
   name        = "${var.project_name}-http-tg"
   target_type = "ip"
-  port        = 3000
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
 
